056a07c6bd54a0abbfeb9b49b24d7026739990f0
Hephesto <u19101678@utp.edu.pe>
2021-10-18 16:54:56 -0500

    esperanza

--- index.php
+++ index.php
@@ -1,217 +1,210 @@
-<!DOCTYPE HTML>
-<!--
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-	<head>
-		<title>Landed by HTML5 UP</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-	</head>
-	<body class="is-preload landing">
-		<div id="page-wrapper">
+<?php
+/**
+ * Index template.
+ *
+ * @package Neve
+ */
 
-			<!-- Header -->
-				<header id="header">
-					<h1 id="logo"><a href="index.html">Landed</a></h1>
-					<nav id="nav">
-						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li>
-								<a href="#">Layouts</a>
-								<ul>
-									<li><a href="left-sidebar.html">Left Sidebar</a></li>
-									<li><a href="right-sidebar.html">Right Sidebar</a></li>
-									<li><a href="no-sidebar.html">No Sidebar</a></li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Option 1</a></li>
-											<li><a href="#">Option 2</a></li>
-											<li><a href="#">Option 3</a></li>
-											<li><a href="#">Option 4</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							<li><a href="elements.html">Elements</a></li>
-							<li><a href="#" class="button primary">Sign Up</a></li>
-						</ul>
-					</nav>
-				</header>
+/**
+ * Filters the container classes.
+ *
+ * @param string $classes Container classes.
+ *
+ * @since 1.0.0
+ */
+$container_class = apply_filters( 'neve_container_class_filter', 'container', 'blog-archive' );
 
-			<!-- Banner -->
-				<section id="banner">
-					<div class="content">
-						<header>
-							<h2>The future has landed</h2>
-							<p>And there are no hoverboards or flying cars.<br />
-							Just apps. Lots of mother flipping apps.</p>
-						</header>
-						<span class="image"><img src="/images/pic01.jpg" alt="" /></span>
-					</div>
-					<a href="#one" class="goto-next scrolly">Next</a>
-				</section>
+get_header();
 
-			<!-- One -->
-				<section id="one" class="spotlight style1 bottom">
-					<span class="image fit main"><img src="images/pic02.jpg" alt="" /></span>
-					<div class="content">
-						<div class="container">
-							<div class="row">
-								<div class="col-4 col-12-medium">
-									<header>
-										<h2>Odio faucibus ipsum integer consequat</h2>
-										<p>Nascetur eu nibh vestibulum amet gravida nascetur praesent</p>
-									</header>
-								</div>
-								<div class="col-4 col-12-medium">
-									<p>Feugiat accumsan lorem eu ac lorem amet sed accumsan donec.
-									Blandit orci porttitor semper. Arcu phasellus tortor enim mi
-									nisi praesent dolor adipiscing. Integer mi sed nascetur cep aliquet
-									augue varius tempus lobortis porttitor accumsan consequat
-									adipiscing lorem dolor.</p>
-								</div>
-								<div class="col-4 col-12-medium">
-									<p>Morbi enim nascetur et placerat lorem sed iaculis neque ante
-									adipiscing adipiscing metus massa. Blandit orci porttitor semper.
-									Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer
-									mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem
-									ipsum dolor nullam.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<a href="#two" class="goto-next scrolly">Next</a>
-				</section>
+$wrapper_classes = [ 'posts-wrapper' ];
+if ( ! neve_is_new_skin() ) {
+	$wrapper_classes[] = 'row';
+}
 
-			<!-- Two -->
-				<section id="two" class="spotlight style2 right">
-					<span class="image fit main"><img src="images/pic03.jpg" alt="" /></span>
-					<div class="content">
-						<header>
-							<h2>Interdum amet non magna accumsan</h2>
-							<p>Nunc commodo accumsan eget id nisi eu col volutpat magna</p>
-						</header>
-						<p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
-						<ul class="actions">
-							<li><a href="#" class="button">Learn More</a></li>
-						</ul>
-					</div>
-					<a href="#three" class="goto-next scrolly">Next</a>
-				</section>
+?>
+	<div class="<?php echo esc_attr( $container_class ); ?> archive-container">
+		<div class="row">
+			<?php
+			/**
+			 * Executes the rendering function for the sidebar.
+			 *
+			 * @param string $context Sidebar context.
+			 * @param string $position Sidebar position.
+			 *
+			 * @since 1.0.0
+			 */
+			do_action( 'neve_do_sidebar', 'blog-archive', 'left' );
+			?>
+			<div class="nv-index-posts blog col">
+				<?php
+				/**
+				 * Executes actions before the posts loop start.
+				 *
+				 * @since 2.4.0
+				 */
+				do_action( 'neve_before_loop' );
 
-			<!-- Three -->
-				<section id="three" class="spotlight style3 left">
-					<span class="image fit main bottom"><img src="images/pic04.jpg" alt="" /></span>
-					<div class="content">
-						<header>
-							<h2>Interdum felis blandit praesent sed augue</h2>
-							<p>Accumsan integer ultricies aliquam vel massa sapien phasellus</p>
-						</header>
-						<p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
-						<ul class="actions">
-							<li><a href="#" class="button">Learn More</a></li>
-						</ul>
-					</div>
-					<a href="#four" class="goto-next scrolly">Next</a>
-				</section>
+				/**
+				 * Executes the rendering function for the page header.
+				 *
+				 * @param string $context Header display page.
+				 *
+				 * @since 2.3.10
+				 */
+				do_action( 'neve_page_header', 'index' );
 
-			<!-- Four -->
-				<section id="four" class="wrapper style1 special fade-up">
-					<div class="container">
-						<header class="major">
-							<h2>Accumsan sed tempus adipiscing blandit</h2>
-							<p>Iaculis ac volutpat vis non enim gravida nisi faucibus posuere arcu consequat</p>
-						</header>
-						<div class="box alt">
-							<div class="row gtr-uniform">
-								<section class="col-4 col-6-medium col-12-xsmall">
-									<span class="icon solid alt major fa-chart-area"></span>
-									<h3>Ipsum sed commodo</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
-								<section class="col-4 col-6-medium col-12-xsmall">
-									<span class="icon solid alt major fa-comment"></span>
-									<h3>Eleifend lorem ornare</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
-								<section class="col-4 col-6-medium col-12-xsmall">
-									<span class="icon solid alt major fa-flask"></span>
-									<h3>Cubilia cep lobortis</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
-								<section class="col-4 col-6-medium col-12-xsmall">
-									<span class="icon solid alt major fa-paper-plane"></span>
-									<h3>Non semper interdum</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
-								<section class="col-4 col-6-medium col-12-xsmall">
-									<span class="icon solid alt major fa-file"></span>
-									<h3>Odio laoreet accumsan</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
-								<section class="col-4 col-6-medium col-12-xsmall">
-									<span class="icon solid alt major fa-lock"></span>
-									<h3>Massa arcu accumsan</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
-							</div>
-						</div>
-						<footer class="major">
-							<ul class="actions special">
-								<li><a href="#" class="button">Magna sed feugiat</a></li>
-							</ul>
-						</footer>
-					</div>
-				</section>
+				/**
+				 * Executes actions before the post loop.
+				 *
+				 * @since 2.3.10
+				 */
+				do_action( 'neve_before_posts_loop' );
 
-			<!-- Five -->
-				<section id="five" class="wrapper style2 special fade">
-					<div class="container">
-						<header>
-							<h2>Magna faucibus lorem diam</h2>
-							<p>Ante metus praesent faucibus ante integer id accumsan eleifend</p>
-						</header>
-						<form method="post" action="#" class="cta">
-							<div class="row gtr-uniform gtr-50">
-								<div class="col-8 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Your Email Address" /></div>
-								<div class="col-4 col-12-xsmall"><input type="submit" value="Get Started" class="fit primary" /></div>
-							</div>
-						</form>
-					</div>
-				</section>
+				$current_post_type = get_post_type();
+				$should_add_hook   = ! in_array( $current_post_type, array( 'post', 'page', 'product' ) );
 
-			<!-- Footer -->
-				<footer id="footer">
-					<ul class="icons">
-						<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-						<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
-						<li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
-					</ul>
-					<ul class="copyright">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-					</ul>
-				</footer>
+				if ( have_posts() ) {
+					/* Start the Loop. */
+					echo '<div class="' . esc_attr( join( ' ', $wrapper_classes ) ) . '">';
 
+
+					$pagination_type = get_theme_mod( 'neve_pagination_type', 'number' );
+					if ( $pagination_type !== 'infinite' ) {
+						global $wp_query;
+
+						$posts_on_current_page = $wp_query->post_count;
+						$hook_after_post       = -1;
+
+						if ( $posts_on_current_page >= 2 ) {
+							$hook_after_post = (int) ceil( $posts_on_current_page / 2 );
+						}
+						$post_index = 1;
+					}
+
+					if ( $should_add_hook ) {
+						/**
+						 * Executes actions before a post loop.
+						 *
+						 * The dynamic portion of the hook name, $current_post_type, refers to the post type slug.
+						 * This hook is not available for the following post types: post, page, product.
+						 *
+						 * Possible action names include:
+						 * - neve_loop_attachment_before
+						 * - neve_loop_acme_product_before
+						 *
+						 * @since 2.11
+						 */
+						do_action( 'neve_loop_' . $current_post_type . '_before' );
+					}
+
+					while ( have_posts() ) {
+						the_post();
+
+						if ( $should_add_hook ) {
+							/**
+							 * Executes actions before a post loop entry.
+							 *
+							 * The dynamic portion of the hook name, $current_post_type, refers to the post type slug.
+							 * This hook is not available for the following post types: post, page, product.
+							 *
+							 * Possible action names include:
+							 * - neve_loop_attachment_entry_before
+							 * - neve_loop_acme_product_entry_before
+							 *
+							 * @since 2.11
+							 */
+							do_action( 'neve_loop_' . $current_post_type . '_entry_before' );
+						}
+
+						/**
+						 * Executes actions before rendering the post content.
+						 *
+						 * @since 2.11
+						 */
+						do_action( 'neve_loop_entry_before' );
+
+						get_template_part( 'template-parts/content', get_post_type() );
+
+						if ( $pagination_type !== 'infinite' ) {
+							if ( $post_index === $hook_after_post && $hook_after_post !== - 1 ) {
+								/**
+								 * Executes actions in the middle of the loop.
+								 *
+								 * The actions only fires if the pagination is not set as "infinite scroll".
+								 *
+								 * @since 2.3.10
+								 */
+								do_action( 'neve_middle_posts_loop' );
+							}
+							$post_index ++;
+						}
+
+						/**
+						 * Executes actions after rendering the post content.
+						 *
+						 * @since 2.11
+						 */
+						do_action( 'neve_loop_entry_after' );
+
+						if ( $should_add_hook ) {
+							/**
+							 * Executes actions after a post loop entry.
+							 *
+							 * The dynamic portion of the hook name, $current_post_type, refers to the post type slug.
+							 * This hook is not available for the following post types: post, page, product.
+							 *
+							 * Possible action names include:
+							 * - neve_loop_attachment_entry_after
+							 * - neve_loop_acme_product_entry_after
+							 *
+							 * @since 2.11
+							 */
+							do_action( 'neve_loop_' . $current_post_type . '_entry_after' );
+						}
+					}
+					echo '</div>';
+					if ( ! is_singular() ) {
+						/**
+						 * Executes the rendering function for the pagination.
+						 *
+						 * @param string $context Pagination location context.
+						 */
+						do_action( 'neve_do_pagination', 'blog-archive' );
+					}
+				} else {
+					get_template_part( 'template-parts/content', 'none' );
+				}
+				?>
+				<div class="w-100"></div>
+				<?php
+				/**
+				 * Executes actions after the post loop.
+				 *
+				 * @since 2.3.10
+				 */
+				do_action( 'neve_after_posts_loop' );
+
+				if ( $should_add_hook ) {
+					/**
+					 * Executes actions after a post loop.
+					 *
+					 * The dynamic portion of the hook name, $current_post_type, refers to the post type slug.
+					 * This hook is not available for the following post types: post, page, product.
+					 *
+					 * Possible action names include:
+					 * - neve_loop_attachment_after
+					 * - neve_loop_acme_product_after
+					 *
+					 * @since 2.11
+					 */
+					do_action( 'neve_loop_' . $current_post_type . '_after' );
+				}
+				?>
+			</div>
+			<?php
+			do_action( 'neve_do_sidebar', 'blog-archive', 'right' );
+			?>
 		</div>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-
-	</body>
-</html>
+	</div>
+<?php
+get_footer();
